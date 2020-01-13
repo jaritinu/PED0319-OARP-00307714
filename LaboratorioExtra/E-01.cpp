863b0f21e4aee9f927f5dfe9fe4a042c3460399e
jaritinu <oscar.arp@hotmail.com>
2020-01-13 13:42:18 -0600

    E-01.cpp

--- LaboratorioExtra/E-01.cpp
+++ LaboratorioExtra/E-01.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+using namespace std;
+
+int main(void)
+{
+    char secret[] = {'c', 'o', 'd', 'i', 'f', 'i', 'c', 'a', 'r'};
+    char verification[9];
+    char guessLetter;
+    int a = 13;
+    bool bandera = true;
+
+    cout << "Ahorcado" << endl;
+    cout << "La palabra a adivinar contiene nueve letras" << endl;
+
+    do
+    {
+        cout << "Letra: ";
+        cin >> guessLetter;
+
+    } while (a > 0);
+
+    return 0;
+}
+
+void verifyLetter(char arr1[9], char arr2[9], char letter, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr1[i] == letter)
+        {
+            arr2[i] = letter;
+        }
+    }
+}
