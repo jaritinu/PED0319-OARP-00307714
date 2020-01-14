7b372f278d5f2490a4968197036be16c4ab87a00
jaritinu <oscar.arp@hotmail.com>
2020-01-13 22:56:41 -0600

    Modify HW-01.cpp

--- Laboratorio 01/HW-01.cpp
+++ Laboratorio 01/HW-01.cpp
@@ -5,10 +5,10 @@ bool checkPrime(int);
 
 int main(void){
     int sumPrimes = 0;
-    bool flag;
+    bool flag=false;
 
     for(int i = 2; i < 100;i++){
-        if(checkPrime(i)){
+        if(checkPrime(i)){//si el valor es verdadero se procedera a sumar el numero primo
             sumPrimes+=i;
         }
     }
@@ -17,12 +17,12 @@ int main(void){
 }
 
 bool checkPrime(int n){
-    bool flag = true;
+    bool flag = true;//se inicializa verdadero porque en la condicion se probara lo contrario
     for(int j = 2; j <= n/2;j++)
     {
-        if (n%j == 0){
+        if ( n%j == 0){
             flag = false;
-            break;
+            break;//termina el ciclo para este numero que no es primo
         }
     }
     return flag;
