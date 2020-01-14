aaf6c6ced1676440d15441fa03749e3783fd6ff2
jaritinu <oscar.arp@hotmail.com>
2020-01-13 22:28:47 -0600

    Modify HW-03.cpp

--- Laboratorio 01/HW-03.cpp
+++ Laboratorio 01/HW-03.cpp
@@ -8,14 +8,15 @@ int main (void){
     string test1 = "micadena";
     string test2 = "m1c4d3n4";
     int n = test1.length();
-    alfa(test2,n);
-    if (alfa == 0){
+    int aux = 0;
+    
+    aux = alfa(test1,n);
+    if (aux == 0){
         cout << "La cadena es alfanumerica"<<endl;
     }
     else{
         cout << "La cadena solamente contiene letras"<<endl;
-    }
-        
+    }       
     return 0;
 }
 
@@ -24,8 +25,6 @@ int alfa(string c, int n){
     char aux=' ';
     for(int i=0;i<n;i++){
         aux=c[i];
-       // cout<<aux;
-       // cout<<int(aux);
         if(aux >='0' &&  aux <= '9' ){
             result=0;
             i=n;
