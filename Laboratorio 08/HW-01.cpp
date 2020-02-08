1978d04318f89414a4a06c09c9e608b986134daa
jaritinu <oscar.arp@hotmail.com.sv>
2020-02-07 22:45:14 -0600

    Add HW-01.cpp

--- Laboratorio 08/HW-01.cpp
+++ Laboratorio 08/HW-01.cpp
@@ -13,8 +13,14 @@ struct node{
     node* left,* right;
 };
 
-void insertInTree(node** tree, string word);
-completeName solicitarDato();
+node* createleaf(completeName cname);
+
+void insertInTree(node** tree, completeName cname);
+void solicitarDato(node * pTree);
+void recorrerArbol(node * pTree);
+void postOrder(node* pTree);
+void preOrder(node* pTree);
+void inOrder(node* pTree);
 
 int main (void){
     node * pTree = NULL;
@@ -30,45 +36,82 @@ int main (void){
         cin >> opcion;
         switch(opcion){
             case 1: solicitarDato(pTree); break;
-            case 2: recorrerArbol(arbol); break;
+            case 2: recorrerArbol(pTree); break;
             case 3: continuar = false; break;
             default: cout << "Opcion erronea!" << endl; break;
         }
     }while(continuar);
 
-
-    insertInTree(&pTree, "Hola");
-    insertInTree(&pTree, "Pupusas");
-    insertInTree(&pTree, "Codigo");
-    insertInTree(&pTree, "Adios");
-    insertInTree(&pTree, "F");
     return 0;
 }
 
-node* createleaf(string word){
+node* createleaf(completeName cname){
     node * leaf = new node;
-    leaf->person.lastName = word;
+    leaf->person = cname;
     leaf->left = NULL;
     leaf->right = NULL;
 
     return leaf; 
 }
 
-void insertInTree(node** tree, string word){
+void insertInTree(node** tree, completeName cname){
     if(!(*tree)){
-        *tree = createleaf(word);
+        *tree = createleaf(cname);
     }
     else{
-        if(word.compare((*(*tree)).person.lastName)<=0)
-            insertInTree(&(*(*tree)).left,word);
+        if(cname.lastName.compare((*(*tree)).person.lastName)<=0)
+            insertInTree(&(*(*tree)).left,cname);
         else
-            insertInTree(&(*(*tree)).right,word);
+            insertInTree(&(*(*tree)).right,cname);
     }
 }
-completeName solicitarDato(){
-    completeName unDato;
+void solicitarDato(node * pTree){
+    completeName cname;
+    cout << "Nombre: " ;    cin >> cname.name; cin.ignore();
+    cout << "\nApellido: " ;    cin >> cname.lastName; cin.ignore();
+    insertInTree(&pTree, cname);
+    
+}
 
-    cin >> unDato.name; cin.ignore();
-    cin >> unDato.lastName; cin.ignore();
-    return unDato;
+void recorrerArbol(node * pTree){
+     cout << "Imprimiendo en preOrder" << endl;
+    preOrder(pTree); cout << endl << endl;
+
+    cout << "Imprimiendo en inOrder" << endl;
+    inOrder(pTree); cout << endl << endl;
+
+    cout << "Imprimiendo en postOrder" << endl;
+    postOrder(pTree); cout << endl << endl;
+    
+}
+
+void postOrder(node* pTree){
+    if(!pTree)
+        return;
+    else{
+        postOrder(pTree->left);
+        postOrder(pTree->right);
+        cout << pTree->person.lastName << " " << pTree->person.name << "\t";
+        
+    }
+}
+
+void preOrder(node* pTree){
+    if(!pTree)
+        return;
+    else{
+        cout << pTree->person.lastName << " " << pTree->person.name << "\t";
+        preOrder(pTree->left);
+        preOrder(pTree->right);
+    }
+}
+
+void inOrder(node* pTree){
+    if(!pTree)
+        return;
+    else{
+        inOrder(pTree->left);
+        cout << pTree->person.lastName << " " << pTree->person.name << "\t";
+        inOrder(pTree->right);
+    }
 }
